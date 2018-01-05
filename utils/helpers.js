573a361949d7cc477bbcde6bb4712b23cd767a77
Chris Bell <chrisbelldevelopment@gmail.com>
2018-01-04 23:08:27 -0500

    Initial commit

---
+++ utils/helpers.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default function getDecks () {
+  // return all of the decks along with their titles, questions, and answers.
+}
+
+export default function getDeck () {
+  // take in a single id argument and return the deck associated with that id
+}
+
+export default function saveDecktitle () {
+  // take in a single title argument and add it to the decks
+}
+
+export default function addCardToDeck () {
+  // take in two arguments, title, and card, and will add the card to the list of questions for the deck with the associated title
+}
